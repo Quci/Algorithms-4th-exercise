74d995d8f0559504581e82c1fecb9f3344b14782
Quci <xujiejiuxi@gmail.com>
2020-09-03 00:41:22 +0800

    demo-1.3.1.7: evaluate by tow stacks

--- src/part1/section1_3/Evaluate.java
+++ src/part1/section1_3/Evaluate.java
@@ -7,8 +7,8 @@ import java.util.Stack;
 
 public class Evaluate {
     public static void main(String[] args) {
-        Stack<String> ops = new Stack<String>();
-        Stack<Double> vals = new Stack<Double>();
+        Stack<String> ops = new Stack<>();
+        Stack<Double> values = new Stack<>();
         while (!StdIn.isEmpty()) {
             // if read string is operator , push it onto ops
             String s = StdIn.readString();
@@ -18,23 +18,26 @@ public class Evaluate {
                 ops.push(s);
             } else if (s.equals(")")) {
                 String op = ops.pop();
-                double v = vals.pop();
+                double v = values.pop();
                 if (op.equals("+")) {
-                    v = vals.pop() + v;
+                    v = values.pop() + v;
                 } else if (op.equals("-")) {
-                    v = vals.pop() - v;
+                    v = values.pop() - v;
                 } else if (op.equals("*")) {
-                    v = vals.pop() * v;
+                    v = values.pop() * v;
                 } else if (op.equals("/")) {
-                    v = vals.pop() / v;
+                    v = values.pop() / v;
                 }
-                vals.push(v);
+                values.push(v);
             } else if(s.equals("end")) {
                 break;
             } else {
-                vals.push(Double.parseDouble(s));
+                values.push(Double.parseDouble(s));
             }
         }
-        StdOut.println(vals.pop());
+        StdOut.println(values.pop());
     }
+
+
+
 }
