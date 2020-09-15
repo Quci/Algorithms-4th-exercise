a3a0d2878a61d69586f0b11a0df5e3a9aaad2393
Quci <xujiejiuxi@gmail.com>
2020-09-15 23:48:39 +0800

    sort algorithms Insertion, Selection, shell

---
+++ src/part2/section2_1/Insertion.java
@@ -0,0 +1,47 @@
+package part2.section2_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Insertion {
+    public static void sort(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (less(a[i], a[i - 1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = "sortexample".split("");
+        sort(a);
+        // to use assert: config VM options -ea
+        assert isSorted(a) : "is not sorted";
+        show(a);
+    }
+}
